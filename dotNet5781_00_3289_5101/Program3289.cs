93230dcc56a03311ed4cef80335b8e12a576a002
shmuel bisberg <sbisberg@g.jct.ac.il>
2020-10-15 11:09:59 +0300

    line 14 have been changed.

--- dotNet5781_00_3289_5101/Program3289.cs
+++ dotNet5781_00_3289_5101/Program3289.cs
@@ -11,7 +11,7 @@ namespace dotNet5781_00_3289_5101
         static void Main(string[] args)
         {
             Console.WriteLine("Good morning");
-            Console.WriteLine("new wworld");
+            Console.WriteLine("new shoose");
             Welcome3289();
             Welcome5101();
             Console.ReadKey();
