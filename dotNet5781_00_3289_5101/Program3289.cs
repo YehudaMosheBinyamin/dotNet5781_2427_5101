d95659e4807f1fb59c463ca8023b694fe8c90a21
yehudaben2@gmail.com <yehudaben2@gmail.com>
2020-10-15 10:03:17 +0300

    Initial code welcome3289

---
+++ dotNet5781_00_3289_5101/Program3289.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3289_5101
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3289();
+            Welcome5101();
+            Console.ReadKey();
+        }
+
+      
+        static partial void Welcome5101();
+
+        private static void Welcome3289()
+        {
+            Console.WriteLine("Enter your name: ");
+            string readName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", readName);
+        }
+    }
+}
