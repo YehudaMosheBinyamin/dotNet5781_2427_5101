270c543c05b2ff4b8ef1c707cc1f22b37b1731d4
yehudaben2@gmail.com <yehudaben2@gmail.com>
2020-10-17 19:35:03 +0300

    solution with correct details of tz

---
+++ dotNet5781_00_2427_5101/Program2427.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_2427_5101
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good morning");
+            Console.WriteLine("new shoose");
+            Welcome2427();
+            Welcome5101();
+            Console.ReadKey();
+        }
+
+
+        static partial void Welcome5101();
+        private static void Welcome2427()
+        {
+            Console.WriteLine("Enter your name: ");
+            string readName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", readName);
+        }
+    }
+}
+
---
+++ dotNet5781_00_2427_5101/Program5101.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_2427_5101
+{
+    partial class Program
+    {
+        static partial void Welcome5101()
+        {
+            Console.WriteLine("I an also here ");
+        }
+    }
+}
+
