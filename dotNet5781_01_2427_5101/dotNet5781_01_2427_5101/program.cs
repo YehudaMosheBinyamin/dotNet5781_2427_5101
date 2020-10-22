729b68133c1882b98b21c30471fc1989e496739f
shmuelbisberg.acc@gmail.com <shmuelbisberg.acc@gmail.com>
2020-10-22 16:56:09 +0300

    הclass Bus הושלם

---
+++ dotNet5781_01_2427_5101/dotNet5781_01_2427_5101/program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_2427_5101
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+
+            DateTime date = new DateTime(2005, 11, 12, 9, 0, 0);
+            Console.WriteLine(date);
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
