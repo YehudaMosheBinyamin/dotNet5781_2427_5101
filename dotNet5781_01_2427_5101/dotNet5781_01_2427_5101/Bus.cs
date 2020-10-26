a1f732457d499779cd73d24fddc0928128335e83
yehudaben2@gmail.com <yehudaben2@gmail.com>
2020-10-26 19:34:15 +0200

    diferent variable KmPossible instead of IfFull

--- dotNet5781_01_2427_5101/dotNet5781_01_2427_5101/Bus.cs
+++ dotNet5781_01_2427_5101/dotNet5781_01_2427_5101/Bus.cs
@@ -13,9 +13,9 @@ namespace dotNet5781_01_2427_5101
         private bool isDangerous; // האם מסוכן
         private DateTime start; // תאריך תחילת שרות
         private DateTime lastTreated; // תאריך טיפול אחרון
-        private int allKmTrav; // קילומטרג
-        private int kmSinsTreat; // קמ מאז טיפול אחרון
-        private int isFull; // האם המיכל מלא בדלק
+        private int allKmTrav; // קילומטרז
+        private int kmSinceTreated; // קמ מאז טיפול אחרון
+        private int kmPossible; // כמה קמ אפשר לנסוע כעת כתלות במצב הדלק
         static DateTime currentDate = new DateTime.now; // משתנה זה מצין את היום הנוכחי
         //-------------------------------------------
         // set/get for  all...
@@ -26,14 +26,14 @@ namespace dotNet5781_01_2427_5101
             {
                 if (value < 0) // אם שלילי אז מקבל חריגה
                 {
-                    throw "Licence Number not corecct! - Negative number not possible.";
+                    throw "Licence Number not correct! - Negative number not possible.";
                 }
                 DateTime reform = new DateTime(2018, 0, 0, 0, 0, 0); // תאריך כניסת הרפורמה על לוחות הרישוי
                 if (start < reform) // אם לפני הרפורמה אז
                 {
                     if (value > 9999999) // אם יש יותר מ7 ספרות אז מקבל חריגה
                     {
-                        throw "Too manny diggets!";
+                        throw "Too many digits!";
                     }
                     licenceNumber = value;
                 }
@@ -41,18 +41,18 @@ namespace dotNet5781_01_2427_5101
                 {
                     if (value > 99999999) // אם יותר מ8 ספרות מקבל חריגה
                     {
-                        throw "Too manny diggets!";
+                        throw "Too many digits!";
                     }
                     licenceNumber = value;
                 }
             }
         }
-        public bool IsDengerous
+        public bool IsDangerous
         {
             get 
             { // אם עברה שנה או ש האוטובוס נסע יותר מ20,000 קמ אז מסוכן 
                 DateTime oneYear = new DateTime(1, 0, 0, 0, 0, 0);
-                if ((kmSinsTreat >= 20000) || (lastTreat - today > oneYear))
+                if ((kmSinceTreat >= 20000) || (lastTreat - today > oneYear))
                 {
                     return true;
                 }
@@ -71,7 +71,7 @@ namespace dotNet5781_01_2427_5101
             {
                 if (value < start) // לא אפשרי שטיפול יהיה לפני שנקנה
                 {
-                    throw "Not passible! - the bus wasn't exist.";
+                    throw "Not possible! - the bus didn't exist yet.";
                 }
                 lastTreat = value;
             }
@@ -83,73 +83,86 @@ namespace dotNet5781_01_2427_5101
             {
                 if (value < allKmTrav) // אי אפשר להוריד ערך מהקילומטראג
                 {
-                    throw "Not passible! - Negative number is not exepteble."
+                    throw "Not possible! - Negative number is not acceptable."
                 }
                 allKmTrav = value;
             }
         }
-        public int KmSinsTreat
+        public int KmSinceTreat
         {
-            get { return kmSinsTreat; }
+            get {return kmSinceTreat;}
             set
             {
                 if (value < 0) // אי אפשר שיהיה ערך קטן מ0 בקילומטראג
                 {
-                    throw "Not passible! - Negative number is not exepteble."
+                    throw "Not possible! - Negative number is not acceptable."
                 }
                 kmSinsTreat = value;
             }
         }
-        public int IsFull
+        public int KmPossible
         {
-            get { return IsFull; }
+            get { return kmPossible; }
             set
             {
-                if (value < 0) 
+               if (value < 0) 
                 {
-                    throw "Not passible! - Negative number is not exepteble."
+                    throw "Not possible! - Negative number is not acceptable."
                 }
                 if ( value > 1200) // אי אפשר לעבור יותר מ1,200 קמ בלי תדלוק
                 {
-                    throw "Not passible! - the way is too long."
+                    throw "Not possible! - the way is too long."
                 }
-                isFull = value;
+               kmPossible = value;
             }
                
         }
         //----------------------------------------
-        //constractor:
+        //constructor:
         public Bus(int _licenceNumber, DateTime _start)
         {
             licenceNumber = _licenceNumber;
             start = new DateTime(_start);
             lastTreat = new DateTime(_start);
             allKmTrav = 0;
-            kmSinsTreat = 0;
-            isFull = 0; 
+            kmSinceTreat = 0;
+            kmPossible=0; 
         }
         //----------------------------------------
         // פונקציות בסיסיות של האוטובוס: תדלוק ,טיפול,נסיע  
         void drive(int km)
         {
-            if (IsDengerous) // אם האוטובוס במצב לא תקין אז אי אפשר לבצעה נסיע
+            if (IsDangerous) // אם האוטובוס במצב לא תקין אז אי אפשר לבצעה נסיע
             {
-                throw "The Bus is in a dengerous stat and isn't avalible."
+                throw "The Bus is in a dangerous state and isn't available."
             }
             allKmTrav += km;
-            kmSinsTreat += km;
-            isFull += km;
+            kmSinceTreat += km;
+            //isFull += km;
+            kmPossible -= km;
         }
         void treatment() // בטיפול מקבלים גם דלק
         {
             lastTreat = DateTime.now;
-            kmSinsTreat = 0;
-            isFull = 0;
+            kmSinceTreat = 0;
+            kmPossible = 0;
         }
         void refueling()
         {
-            isFull = 0;
+            KmPossible(1200);
         }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+       
         //------------------------------------------
     }
 }
--- dotNet5781_01_2427_5101/dotNet5781_01_2427_5101/program.cs
+++ dotNet5781_01_2427_5101/dotNet5781_01_2427_5101/program.cs
@@ -2,27 +2,82 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace dotNet5781_01_2427_5101
 {
-
 
     enum choice
     {
         EXIT,ADD,TRAVEL,TREATMENT,SHOW
     }
     class program
-    {treatment
-        }
+    {
+      
         static void Main(string[] arg)
         {int choice;
-         Console.WriteLine("For add a bus enter 1 to travel-2 for treatment-3 for show -4 for exit-0");
+         List<Bus> busList=new List<Bus>();
+         
+        do{Console.WriteLine("For add a bus enter 1 to travel-2 for treatment-3 for show -4 for exit-0");
          choice=Console.ReadLine();
+         int licensePlate;
+          switch(choice)
+            {
+            case EXIT:
+                break;
+            case ADD:Console.WriteLine("Enter license plate and date of start");
+                licensePlate=Console.ReadLine();
+                DateTime currentTime=Console.ReadLine();
+                busList.Add(new Bus(licensePlate,currentTime));
+                break;
+            case TRAVEL:Console.WriteLine("Insert license number of bus");
+                    licensePlate=Console.ReadLine();
+                     Random r=new Random(DateTime.Now.Milisecond);//generates random number 
+                    int randomKmJourney=r.Next(1201);//random number for trip's length
+                    int indexBus=busList.FindIndex(licensePlate);
+                   if(indexBus==-1)
+                    {
+                     Console.WriteLine("This bus doesn't exist,try again");
+                     continue;
+                    }
+                    
+                    if(busList[indexBus].PossibleKm<randomKmJourney)
+                    {
+                        Console.WriteLine("Bus doesn't have enough gas for journey");
+                        continue;
+                    }
+                    if(busList[indexBus].IsDangerous)
+                        {
+                    Console.WriteLine("Bus is dangerous cannot be taken for journey");
+                    continue;}
+                    busList[indexBus].travel();
+                    break;
+           case TREATMENT:
+                    Console.WriteLine("Enter license plate and date of start");
+                    licensePlate=Console.ReadLine();
+                    Console.WriteLine("Insert 0 to refill, any other number  to maintain");
+                    int refillOrMaintain=Console.ReadLine();
+                     indexBus=busList.FindIndex(refillOrMaintain);
+                        if (refillOrMaintain==0)
+                        {
+                         busList[indexBus].refill();
+                        }
+                        else{
+                             busList[indexBus].treatment();    
+                            }
+                        break;
+           case SHOW:
+                    Console.WriteLine("List of buses and their kilometrage since their last treatment");
+                    foreach(Bus bus in busList)
+                     {Console.WriteLine("License number: {} Km since treated: {} ",bus.LicenseNumber,bus.KmSinceTreat);
+                           
+                      }   
+                    break;
+            
+          }
 
-        }
-
-
-
+        }while(choice!=0);
     }
+}
 }
