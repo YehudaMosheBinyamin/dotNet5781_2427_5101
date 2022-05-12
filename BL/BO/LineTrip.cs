208de0d822dba5b2ea21f79727e7b8256c0ac3c5
YehudaMosheBinyamin <62666416+YehudaMosheBinyamin@users.noreply.github.com>
2022-05-12 13:08:32 +0300

    Update LineTrip.cs

--- BL/BO/LineTrip.cs
+++ BL/BO/LineTrip.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BO
 {
+    /*A class that represents a scheduled bus line departure at a specific time*/
     public class LineTrip
     {
         public int Id { get; set; }
