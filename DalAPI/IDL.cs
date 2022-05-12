677993ba9fe7995e93da1991872bf79f5d1426b8
YehudaMosheBinyamin <62666416+YehudaMosheBinyamin@users.noreply.github.com>
2022-05-12 14:19:50 +0300

    Update IDL.cs

--- DalAPI/IDL.cs
+++ DalAPI/IDL.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using DO;
@@ -6,22 +6,12 @@ namespace DalAPI
 {
     //CRUD Logic
     public interface IDL
-    {/**
-        #region Bus
-         void AddBus(Bus bus);
-         IEnumerable<Bus> GetAllBuses();
-         IEnumerable<Bus> GetAllBusesBy(Predicate<Bus> predicate);
-         Bus GetBus(string licenseNum);
-         void UpdateBus(string licenseNum,Action<Bus>update);
-         void DeleteBus(string licenseNum);
-        #endregion
-        **/
+    {
         #region Line
         void AddLine(Line line);
         IEnumerable<Line> GetAllLines();
         IEnumerable<Line> GetAllLinesBy(Predicate<Line> predicate);
         Line GetLine(int lineId);
-        //void UpdateLine(int lineId,Action<Line>update);
         void DeleteLine(int lineId);
         int GetNewLineId();
         #endregion
@@ -30,19 +20,8 @@ namespace DalAPI
         void AddStation(Station station);
         IEnumerable<Station> GetAllStations();
         Station GetStation(int code);
-        //void UpdateStation(Line line, Action<Line> update);
-        //void DeleteStation(int code);
         #endregion
         
-        //#region User
-        //void AddUser(User user);
-        //IEnumerable<User> GetAllUsers();
-        User GetUser(string usernName);
-        //void UpdateUser(string userName, Action<User> update);
-        //void DeleteUser(string userName);
-        // bool CheckUser(string userName, string password);
-        //#endregion
-      
         #region LineTrip
         void AddLineTrip(LineTrip lineTrip);
         IEnumerable<LineTrip> GetAllLineTrips();
@@ -58,7 +37,6 @@ namespace DalAPI
         IEnumerable<AdjacentStations> GetAllAdjacentStations();
         IEnumerable<AdjacentStations> GetAdjacentStationsBy(Predicate<AdjacentStations> predicate);
         AdjacentStations GetAdjacentStations(int stationOneCode,int stationTwoCode);
-        //void UpdateAdjacentStations(int stationOneCode,int stationTwoCode,Action<AdjacentStations>update);------
         void DeleteAdjacentStations(int stationOneCode,int stationTwoCode);
         
         #endregion
@@ -67,7 +45,6 @@ namespace DalAPI
         IEnumerable<LineStation> GetAllLineStations();
         IEnumerable<LineStation> GetAllLineStationsByLine(int lineId);
         LineStation GetLineStation(int lineId,int stationCode);
-        //void UpdateLineStation(int lineId,int stationCode,int newStationCode,Action<LineStation,int>update);
         void DeleteLineStation(int lineId,int stationCode);
         void DeleteLineStations(int lineId);
         bool InLineStations(int station1, int station2);
